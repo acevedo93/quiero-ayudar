0a4c0cfc68f071127da4969643f61a582cca3716
david acevedo <dacevedojaramillo@gmail.com>
2018-11-07 03:09:57 -0300

    add redux

--- src/components/Header_Footer/header.js
+++ src/components/Header_Footer/header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee,faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import Nav from './sidenav'
 
 class Header extends Component {
--- src/index.js
+++ src/index.js
@@ -2,13 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import Routes from './routes'
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
+import promiseMiddleware from 'redux-promise'
+import ReduxThunk from 'redux-thunk'
+import rootReducer from './redux/reducers'
 import './css/style.css'
 
+
 const root = document.getElementById('root');
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware,ReduxThunk)(createStore)
+const store = createStoreWithMiddleware(rootReducer)
+
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Routes/>
-    </BrowserRouter>
+    <Provider store = {store}>
+        <BrowserRouter>
+            <Routes/>
+        </BrowserRouter>
+    </Provider>
     ,root 
 )
